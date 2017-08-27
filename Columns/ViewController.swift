9bfcd4c1ba54575e866eb1e7b5e6225864eb75b0
Greg Sutton <greg.sutton@adlerithech.com>
2017-08-28 10:48:06 +1200

    Initial Commit

---
+++ Columns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Columns
+//
+//  Created by Greg Sutton on 28/08/2017.
+//  Copyright © 2017 Darksheep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
